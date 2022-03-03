e4300a84af41e733f18dbabb5ea4bf1b6a45c368
merunno <merunno92@gmail.com>
2022-03-04 04:09:29 +0900

    remove: project buttonの削除

--- src/component/button.component.tsx
+++ src/component/button.component.tsx
@@ -17,16 +17,6 @@ export default function ButtonC() {
               Contacts
             </Button>
           </Link>
-          {/* TODO: いつかポートフォリオ内に移植する : https://github.com/merunno/merunno.dev/issues/85 */}
-          <a
-            href="https://m92n.notion.site/80be3825bc264434a2799a5bb5586caf?v=2ae5b85320e549a2bae7c4496d53a48b"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Button colorScheme="teal" variant="ghost">
-              Projects
-            </Button>
-          </a>
         </Circle>
       </div>
     </main>
